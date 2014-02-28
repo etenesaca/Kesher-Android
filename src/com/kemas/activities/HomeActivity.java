f9ce1ef26e5d54c950733bde639cf72cf65cd781
etenesaca <edgartenesaca@gmail.com>
2014-02-27 22:59:16 -0500

    Implementado un Drawer layout para utilizarlo como menu

--- src/com/kemas/activities/HomeActivity.java
+++ src/com/kemas/activities/HomeActivity.java
@@ -1,17 +1,97 @@
 package com.kemas.activities;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
 import com.kemas.R;
 
-import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
-
+@SuppressLint("NewApi")
 public class HomeActivity extends Activity {
+
+	private DrawerLayout drawerLayout;
+	private ListView drawer;
+	private ActionBarDrawerToggle toggle;
+	private static final String[] opciones = { "Configurar Conexión", "Mi Datos", "Eventos" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_home);
+
+		// Rescatamos el Action Bar y activamos el boton Home
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setHomeButtonEnabled(true);
+
+		// Declarar e inicializar componentes para el Navigation Drawer
+		drawer = (ListView) findViewById(R.id.drawer);
+		drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+		// Declarar adapter y eventos al hacer click
+		drawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, opciones));
+
+		drawer.setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Toast.makeText(HomeActivity.this, "Pulsado: " + opciones[arg2], Toast.LENGTH_SHORT).show();
+				drawerLayout.closeDrawers();
+				// Para ir a la ventana se Configuraciones
+				Intent config_act2 = new Intent(HomeActivity.this, CollaboratorActivity.class);
+				startActivity(config_act2);
+			}
+		});
+
+		// Sombra del panel Navigation Drawer
+		drawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+		// Integracion boton oficial
+		toggle = new ActionBarDrawerToggle(this, // Activity
+				drawerLayout, // Panel del Navigation Drawer
+				R.drawable.ic_drawer, // Icono que va a utilizar
+				R.string.app_name, // Descripcion al abrir el drawer
+				R.string.hello_world // Descripcion al cerrar el drawer
+		) {
+			public void onDrawerClosed(View view) {
+				// Drawer cerrado
+				getActionBar().setTitle(getResources().getString(R.string.app_name));
+				invalidateOptionsMenu();
+			}
+
+			public void onDrawerOpened(View drawerView) {
+				// Drawer abierto
+				getActionBar().setTitle("Menu");
+				invalidateOptionsMenu();
+			}
+		};
+
+		drawerLayout.setDrawerListener(toggle);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		if (toggle.onOptionsItemSelected(item)) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	// Activamos el toggle con el icono
+	@Override
+	protected void onPostCreate(Bundle savedInstanceState) {
+		super.onPostCreate(savedInstanceState);
+		toggle.syncState();
 	}
 
 	@Override
