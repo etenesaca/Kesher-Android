18601d6608f9cad6632eefa9b154bc940f6450ee
etenesaca <edgartenesaca@gmail.com>
2014-02-27 19:40:18 -0500

    Agragando Activities
    Se agrgo dos activities: Uno para al conexión con el sevidor de OpenERP
    y otro para ver los datos del colaborador

---
+++ src/com/kemas/activities/CollaboratorActivity.java
@@ -0,0 +1,15 @@
+package com.kemas.activities;
+
+import com.kemas.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CollaboratorActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_collaborator);
+	}
+}
---
+++ src/com/kemas/activities/ConnectionActivity.java
@@ -0,0 +1,14 @@
+package com.kemas.activities;
+
+import com.kemas.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ConnectionActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_connection);
+	}
+}
