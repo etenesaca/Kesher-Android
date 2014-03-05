575dae46e6c03b993c8d40d5c2b0e9a010b2dc4e
etenesaca <edgartenesaca@gmail.com>
2014-03-05 14:17:47 -0500

    Ahora los datos del colaborador se cargan en background

--- src/com/kemas/activities/CollaboratorActivity.java
+++ src/com/kemas/activities/CollaboratorActivity.java
@@ -4,20 +4,23 @@ import java.util.HashMap;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.kemas.Configuration;
 import com.kemas.OpenERPconn;
 import com.kemas.R;
-import com.kemas.hupernikao;
 
 @TargetApi(Build.VERSION_CODES.GINGERBREAD)
 @SuppressLint("NewApi")
@@ -26,6 +29,7 @@ public class CollaboratorActivity extends ActionBarActivity {
 	Context Context = (Context) this;
 
 	private TextView txtName;
+	private LinearLayout Contenedor;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -43,34 +47,10 @@ public class CollaboratorActivity extends ActionBarActivity {
 		// Crear una instancia de la Clase de Configuraciones
 		config = new Configuration(this);
 
-		// Cargar los datos del colbarador
+		// Cargar los datos del colaborador
 		txtName = (TextView) findViewById(R.id.txtName);
-		load_collaborator_info();
-	}
-
-	void load_collaborator_info() {
-		if (!hupernikao.TestNetwork(Context)) {
-			Toast msg = Toast.makeText(this, "No se puede Establecer conexión. Revise su conexión a Internet.", Toast.LENGTH_SHORT);
-			msg.show();
-			return;
-		}
-		int Port = Integer.parseInt(config.getPort().toString());
-		String Server = config.getServer().toString();
-		boolean TestConnection = OpenERPconn.TestConnection(Server, Port);
-		if (TestConnection) {
-			OpenERPconn oerp = OpenERPconn.connect(Server, Port, config.getDataBase(), config.getLogin(), config.getPassword());
-			HashMap<String, Object> Collaborator = oerp.getCollaborator(Integer.parseInt(config.getCollaboratorID().toString()));
-			if (Collaborator != null) {
-				txtName.setText(Collaborator.get("name") + "");
-			} else {
-				Toast.makeText(this, "No se pudieron recuperar los datos.", Toast.LENGTH_SHORT).show();
-				finish();
-			}
-
-		} else {
-			Toast msg = Toast.makeText(this, "No se pudo conectar al servidor, Verifique los parametros de Conexión.", Toast.LENGTH_SHORT);
-			msg.show();
-		}
+		Contenedor = (LinearLayout) findViewById(R.id.Contenedor);
+		new LoadInfo(Context).execute();
 	}
 
 	void edit() {
@@ -97,4 +77,53 @@ public class CollaboratorActivity extends ActionBarActivity {
 		return true;
 	}
 
+	class LoadInfo extends AsyncTask<Integer, Void, Integer> {
+		Context context;
+		ProgressDialog pDialog;
+		HashMap<String, Object> Collaborator = null;
+
+		public LoadInfo(Context context) {
+			this.context = context;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			
+			Contenedor.setVisibility(View.INVISIBLE);
+			pDialog = new ProgressDialog(context);
+			pDialog.setMessage("Cargando Datos");
+			pDialog.setCancelable(true);
+			pDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+			pDialog.show();
+		}
+
+		@Override
+		protected Integer doInBackground(Integer... params) {
+			int Port = Integer.parseInt(config.getPort().toString());
+			String Server = config.getServer().toString();
+
+			boolean TestConnection = OpenERPconn.TestConnection(Server, Port);
+			if (TestConnection) {
+				OpenERPconn oerp = OpenERPconn.connect(Server, Port, config.getDataBase(), config.getLogin(), config.getPassword());
+				Collaborator = oerp.getCollaborator(Integer.parseInt(config.getCollaboratorID().toString()));
+			}
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Integer result) {
+			super.onPostExecute(result);
+
+			if (Collaborator != null) {
+				txtName.setText(Collaborator.get("name").toString());
+				Contenedor.setVisibility(View.VISIBLE);
+			} else {
+				Toast.makeText(CollaboratorActivity.this, "No se pudieron recuperar los datos.", Toast.LENGTH_SHORT).show();
+				finish();
+			}
+			pDialog.dismiss();
+		}
+
+	}
 }
