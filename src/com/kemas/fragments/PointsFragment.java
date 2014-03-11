ccb4d91bc7265b116f5dcab35adbe9d8e3694244
etenesaca <edgartenesaca@gmail.com>
2014-03-11 17:03:41 -0500

    Agregado un paquete para los fragmentos

---
+++ src/com/kemas/fragments/PointsFragment.java
@@ -0,0 +1,26 @@
+package com.kemas.fragments;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Fragment;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kemas.R;
+
+/*  Fragment para seccion perfil */
+@SuppressLint("NewApi")
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+public class PointsFragment extends Fragment {
+	public PointsFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_points, container, false);
+		return rootView;
+	}
+}
