a52186740ed0f87a5544f39d6824210d8bbe2b30
etenesaca <edgartenesaca@gmail.com>
2014-04-04 16:07:58 -0500

    Optimizada el scroll de los registros de asistencia

--- src/com/kemas/fragments/AttendancesFragment.java
+++ src/com/kemas/fragments/AttendancesFragment.java
@@ -243,5 +243,4 @@ public class AttendancesFragment extends Fragment {
 			loading = false;
 		}
 	}
-
 }
--- src/com/kemas/item/adapters/AttendancesItemAdapter.java
+++ src/com/kemas/item/adapters/AttendancesItemAdapter.java
@@ -23,6 +23,8 @@ import java.util.List;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -42,11 +44,15 @@ import com.kemas.R;
 public class AttendancesItemAdapter extends ArrayAdapter<HashMap<String, Object>> {
 	private LayoutInflater layoutInflater;
 	private Context CTX;
+	Typeface Roboto_Bold;
+	Typeface Roboto_Light;
 
 	public AttendancesItemAdapter(Context context, List<HashMap<String, Object>> objects) {
 		super(context, 0, objects);
 		this.CTX = context;
 		layoutInflater = LayoutInflater.from(context);
+		Roboto_Bold = Typeface.createFromAsset(CTX.getAssets(), "fonts/Roboto-Bold.ttf");
+		Roboto_Light = Typeface.createFromAsset(CTX.getAssets(), "fonts/Roboto-Light.ttf");
 	}
 
 	@Override
@@ -63,33 +69,84 @@ public class AttendancesItemAdapter extends ArrayAdapter<HashMap<String, Object>
 
 		HashMap<String, Object> Record = getItem(position);
 
-		TextView tvService = (TextView) convertView.findViewById(R.id.tvService);
-		TextView tvNumber = (TextView) convertView.findViewById(R.id.tvNumber);
-		TextView tvType = (TextView) convertView.findViewById(R.id.tvType);
-		TextView tvDate = (TextView) convertView.findViewById(R.id.tvDate);
-		TextView tvHour = (TextView) convertView.findViewById(R.id.tvHour);
-		TextView tvDay = (TextView) convertView.findViewById(R.id.tvDay);
-
-		Typeface Roboto_Light = Typeface.createFromAsset(CTX.getAssets(), "fonts/Roboto-Light.ttf");
-		Typeface Roboto_Bold = Typeface.createFromAsset(CTX.getAssets(), "fonts/Roboto-Bold.ttf");
-
-		tvService.setTypeface(Roboto_Bold);
-		tvService.setText(Record.get("service").toString());
-		tvNumber.setText("#" + Record.get("id").toString());
-		tvDate.setText(Record.get("date").toString());
-		tvHour.setText(Record.get("hour").toString());
-		tvDay.setText(Record.get("day").toString());
-		tvDay.setTypeface(Roboto_Light);
-		if ((Record.get("type").toString()).equals("just_time")) {
-			tvType.setText("A Tiempo");
-			tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_atiempo));
-		} else if ((Record.get("type").toString()).equals("late")) {
-			tvType.setText("Atrazo");
-			tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_tarde));
+		// Ejecutar la Tarea de acuerdo a la version de Android
+		LoadView Task = new LoadView(convertView, Record);
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			Task.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 		} else {
-			tvType.setText("Insistencia");
-			tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_falta));
+			Task.execute();
 		}
 		return convertView;
+	}
+
+	/** Clase Asincrona para recuperar los datos de la fila **/
+	protected class LoadView extends AsyncTask<String, Void, String> {
+		HashMap<String, Object> Record;
+		View convertView;
+
+		TextView tvService;
+		TextView tvNumber;
+		TextView tvType;
+		TextView tvDate;
+		TextView tvHour;
+		TextView tvDay;
+
+		public LoadView(View convertView, HashMap<String, Object> Record) {
+			this.Record = Record;
+			this.convertView = convertView;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+
+			tvService = (TextView) convertView.findViewById(R.id.tvService);
+			tvNumber = (TextView) convertView.findViewById(R.id.tvNumber);
+			tvType = (TextView) convertView.findViewById(R.id.tvType);
+			tvDate = (TextView) convertView.findViewById(R.id.tvDate);
+			tvHour = (TextView) convertView.findViewById(R.id.tvHour);
+			tvDay = (TextView) convertView.findViewById(R.id.tvDay);
+
+			tvService.setVisibility(View.INVISIBLE);
+			tvNumber.setVisibility(View.INVISIBLE);
+			tvType.setVisibility(View.INVISIBLE);
+			tvDate.setVisibility(View.INVISIBLE);
+			tvHour.setVisibility(View.INVISIBLE);
+			tvDay.setVisibility(View.INVISIBLE);
+		}
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			tvService.setTypeface(Roboto_Bold);
+			tvService.setText(Record.get("service").toString());
+
+			tvNumber.setText("#" + Record.get("id").toString());
+			tvDate.setText(Record.get("date").toString());
+			tvHour.setText(Record.get("hour").toString());
+			tvDay.setText(Record.get("day").toString());
+			tvDay.setTypeface(Roboto_Light);
+			if ((Record.get("type").toString()).equals("just_time")) {
+				tvType.setText("A Tiempo");
+				tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_atiempo));
+			} else if ((Record.get("type").toString()).equals("late")) {
+				tvType.setText("Atrazo");
+				tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_tarde));
+			} else {
+				tvType.setText("Insistencia");
+				tvType.setBackgroundDrawable(CTX.getResources().getDrawable(R.drawable.shape_falta));
+			}
+
+			tvService.setVisibility(View.VISIBLE);
+			tvNumber.setVisibility(View.VISIBLE);
+			tvType.setVisibility(View.VISIBLE);
+			tvDate.setVisibility(View.VISIBLE);
+			tvHour.setVisibility(View.VISIBLE);
+			tvDay.setVisibility(View.VISIBLE);
+		}
 	}
 }
